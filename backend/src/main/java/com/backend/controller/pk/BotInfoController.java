afb8d2249c5fea91eb46f776e0b89c6b584ccf65
jinli wei <wllworkemail@163.com>
2022-07-08 18:18:22 +0800

    backend index shows test

--- backend/src/main/java/com/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/backend/controller/pk/BotInfoController.java
@@ -1,4 +1,4 @@
-package com.backend.backend.controller.pk;
+package com.backend.controller.pk;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
--- backend/src/main/java/com/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/backend/controller/pk/IndexController.java
@@ -1,4 +1,4 @@
-package com.backend.backend.controller.pk;
+package com.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
